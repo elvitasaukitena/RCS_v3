a777c6d6a6c1f2eaa91149e7a7a42340c12db0fa
Elvita <elvita.saukitena@inbox.lv>
2018-07-02 19:46:06 +0300

    Day 6

---
+++ Day 6/Day 6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repeat pirmais = new Repeat();
+            pirmais.Izvade();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 6/Day 6/Repeat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6
+{
+    class Repeat
+    {
+        public void Izvade()
+        {
+            Console.WriteLine("Lūdzu, ievadiet skaitli!");
+            string ievade = Console.ReadLine();
+            int viens = Convert.ToInt16(ievade);
+            string output = Sharp(viens);
+            Console.WriteLine("Skaitlis ir " + output);
+        }
+        private string Sharp(int a)
+        {
+            string result = "";
+            if (a > 5)
+            {
+                result = "##";
+            }
+            else
+            {
+                result = "#";
+            }
+            return result; 
+        }
+    }
+}
