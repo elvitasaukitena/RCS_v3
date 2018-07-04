073c0ff0c6f24e2ab3b0e0d3172a2e1ec4b9e938
Elvita <elvita.saukitena@inbox.lv>
2018-07-04 19:47:54 +0300

    Day 8

---
+++ Day 8/Day 8/CikluUzdevumi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8
+{
+    class CikluUzdevumi
+    {
+        public void Zvaigznite()
+        {
+            Console.WriteLine("Ievadiet skaitli!");
+            string input = Console.ReadLine();
+            int robeza = Convert.ToInt16(input);
+            string zvaigzne = "";
+            if(robeza <= 0)
+                {
+                Console.WriteLine("Kļūdaina ievade!");
+            }
+            else
+            {
+                for (int i = 0; i < robeza; i++)
+                {
+                    zvaigzne = zvaigzne + "*";
+                    Console.WriteLine(zvaigzne);
+                }
+            }
+            int skaititajs = 0;
+            zvaigzne = "";
+            while (skaititajs != robeza)
+            {
+                zvaigzne = zvaigzne + "*";
+                skaititajs++;
+            }
+        }
+    }
+}
---
+++ Day 8/Day 8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CikluUzdevumi sis = new CikluUzdevumi();
+            string input = "";
+            while (input != "0")
+            {
+                Console.WriteLine("Izveleties uzdevumu");
+                Console.WriteLine("1 - cikli");
+                Console.WriteLine("0 - iziet");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        sis.Zvaigznite();
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Nepareiza ievade!");
+                        break;
+
+                }
+            }
+        }
+
+    }
+}
